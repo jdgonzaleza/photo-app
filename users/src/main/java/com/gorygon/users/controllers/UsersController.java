2a729d7e062e3415b1b931e2dc5de6e19ac8bd5e
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2020-06-18 17:58:09 -0500

    added Zuul

--- users/src/main/java/com/gorygon/users/controllers/UsersController.java
+++ users/src/main/java/com/gorygon/users/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.gorygon.users.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+	@Autowired
+	private Environment env;
+
 	@RequestMapping(value = "/status/check", method = RequestMethod.GET)
 	public String status() {
-		return "check";
+		return "working on " + env.getProperty("local.server.port");
 	}
 }
